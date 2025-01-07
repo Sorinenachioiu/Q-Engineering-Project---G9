fdf4929de32b918f75b822d84933139a7f2bb63c
Sorin Enăchioiu <s.c.enachioiu@student.tudelft.nl>
2025-01-08 00:23:10 +0100

    Updated the Dockerfile thanks to Eduard Mihaius, copil ghidus

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from connect import get_backend
 
 # set as "ibm" to use ibm backend
-backend_type = "ibm"
-# backend_type = "qi"
+# backend_type = "ibm"
+backend_type = "qi"
 
 backend = get_backend(backend_type, True)

68770f912baadb82f0f2a6928e7a3cc0be52907c
Sorin Enăchioiu <s.c.enachioiu@student.tudelft.nl>
2025-01-08 22:07:46 +0100

    Added a script to run docker commands easier.

---
+++ src/run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Build the Docker image
+docker build -t q_eng_proj .
+
+REM Run the Docker container and map the output folder to ./circuits
+docker run --rm -it -v "%cd%/circuits:/app/output" q_eng_proj
+
+REM Notify the user
+echo Circuit diagram saved to ./circuits/circuit.png

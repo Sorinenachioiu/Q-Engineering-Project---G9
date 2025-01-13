0f32e4dbeb7e84bb7a06f35317dc7f98a35c6530
Sorin Enăchioiu <s.c.enachioiu@student.tudelft.nl>
2025-01-13 13:28:43 +0100

    added Shor_codes

--- src/error_correction/basic_codes.py
+++ src/error_correction/basic_codes.py
@@ -14,8 +14,10 @@ def bit_flip_code(errors, verbose=False):
     circuit.cx(q[0],q[1])
     circuit.cx(q[0],q[2])
 
+    ####error here############
     circuit = apply_errors(circuit, errors)
-    
+    ############################
+
     circuit.cx(q[0],q[1])
     circuit.cx(q[0],q[2])
     circuit.ccx(q[2],q[1],q[0])
@@ -37,7 +39,9 @@ def phase_flip_code(errors, verbose=False):
     circuit.h(q[1])
     circuit.h(q[2]) 
     
+    ####error here############
     circuit = apply_errors(circuit, errors)
+    ############################
 
     circuit.h(q[0])
     circuit.h(q[1])
@@ -47,4 +51,55 @@ def phase_flip_code(errors, verbose=False):
     circuit.cx(q[0],q[2])
     circuit.ccx(q[2],q[1],q[0])
     circuit.measure(q[0],c[0])
+    return circuit
+
+def shor_code(errors, verbose=False):
+    q = QuantumRegister(9,'q')
+    c = ClassicalRegister(1,'c')
+
+    circuit = QuantumCircuit(q,c)
+
+    circuit.cx(q[0],q[3])
+    circuit.cx(q[0],q[6])
+
+    circuit.h(q[0])
+    circuit.h(q[3])
+    circuit.h(q[6])
+
+    circuit.cx(q[0],q[1])
+    circuit.cx(q[3],q[4])
+    circuit.cx(q[6],q[7])
+
+    circuit.cx(q[0],q[2])
+    circuit.cx(q[3],q[5])
+    circuit.cx(q[6],q[8])
+
+    ####error here############
+    circuit = apply_errors(circuit, errors)
+    ############################
+
+    circuit.cx(q[0],q[1])
+    circuit.cx(q[3],q[4])
+    circuit.cx(q[6],q[7])
+
+    circuit.cx(q[0],q[2])
+    circuit.cx(q[3],q[5])
+    circuit.cx(q[6],q[8])
+
+    circuit.ccx(q[1],q[2],q[0])
+    circuit.ccx(q[4],q[5],q[3])
+    circuit.ccx(q[8],q[7],q[6])
+
+    circuit.h(q[0])
+    circuit.h(q[3])
+    circuit.h(q[6])
+
+    circuit.cx(q[0],q[3])
+    circuit.cx(q[0],q[6])
+    circuit.ccx(q[6],q[3],q[0])
+
+    circuit.barrier(q)
+
+    circuit.measure(q[0],c[0])
+
     return circuit
--- src/error_correction/examples/examples.py
+++ src/error_correction/examples/examples.py
@@ -8,16 +8,21 @@ def basic_examples_simple_codes(backend):
 
 def bit_flip_examples(backend):
     errors = [("x", 0)]
-    perform_experiment(backend, bit_flip_code(errors, True), "errors/bit_flip0")
+    perform_experiment(backend, bit_flip_code(errors, True), "errors/basic/bit_flip0")
 
     errors = [("x", 1)]
-    perform_experiment(backend, bit_flip_code(errors, True), "errors/bit_flip1")
+    perform_experiment(backend, bit_flip_code(errors, True), "errors/basic/bit_flip1")
 
     errors = [("x", 1), ("x", 0)]
-    perform_experiment(backend, bit_flip_code(errors, True), "errors/bit_flip_0_1")
+    perform_experiment(backend, bit_flip_code(errors, True), "errors/basic/bit_flip_0_1")
 
     errors = [("z", 1)]
-    perform_experiment(backend, bit_flip_code(errors, True), "errors/phase_flip")
+    perform_experiment(backend, bit_flip_code(errors, True), "errors/basic/phase_flip")
 
     errors = [("y", 2)]
-    perform_experiment(backend, bit_flip_code(errors, True), "errors/bit_phase_flip")
+    perform_experiment(backend, bit_flip_code(errors, True), "errors/basic/bit_phase_flip")
+
+
+def shor_code_examples(backend):
+    errors = [("y", 0)]
+    perform_experiment(backend, shor_code(errors, True), "errors/shor/shor_x_flip")

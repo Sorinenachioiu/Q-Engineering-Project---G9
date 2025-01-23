05646efe53cede4476a38ea54d0e2fa46dd64866
Sorin Enăchioiu <s.c.enachioiu@student.tudelft.nl>
2025-01-23 06:56:32 +0100

    made main more _spicy_ added calls to deutsch with logical qubits and also the error simulation, and also interpreting the results of ibm quantum

--- src/main.py
+++ src/main.py
@@ -4,23 +4,57 @@ from error_correction.examples.examples import *
 from surface_codes.Steane import *
 from surface_codes.four_two_two import *
 from experiments.run_experiments import *
-
-# from basic_tic_tac_toe import *
+from run_ibm.four_two_two.four_two_two_ibm import *
+from run_ibm.four_two_two.Deutsch import *
 
 # set as "ibm" to use ibm backend
 # backend_type = "ibm"
 backend_type = "qi"
-# hi this is fun
+
 backend = get_backend(backend_type, True)
+
+
+# if backend is None:
+#     raise ValueError("Failed to retrieve IBM backend. Please check IBM credentials and configuration.")
+# else:
+#     run_422_on_ibm(backend)
+
+# from run_ibm.four_two_two.results.interpret import *
+# decode_measurement_results()
+
+
+# Below to run all the deutsch logical 422 
+from run_ibm.four_two_two.four_two_two_qi import *
+run_Deutsch_logical_422_qi(backend)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # run_initial_examples(backend) # - run universal gate set and Grover
 # basic_examples_simple_codes(backend) # - run error correction examples
 # shor_code_examples(backend)
 
 # counts = perform_experiment(backend, steane_code(), "surface/steane", shots=100)
-
 # results = analyze_Steane_logical_state(counts)
-
 # pretty_print_Steane_results(results)
 
-run_experiments(backend)
+# run_experiments(backend)
